c99beadf5f6a703ec2208daee9be06737a887810
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-16 14:58:32 +0330

    Update Register.v

--- Register.v
+++ Register.v
@@ -2,15 +2,15 @@
 module Register
 
   parameter n = 64
-  input [n-1 : 0] D,
-  input rst,
+  input [n-1 : 0] data_input,
+  input reset,
   input clk,
-  output reg [n-1 : 0] Q
+  output reg [n-1 : 0] data_output
 
   always @(posedge clk)begin
     if (reset)
-      Q <= {n{1'b0}};
+      data_output <= {n{1'b0}};
     else
-      Q <= D;   
+      data_output <= data_input;   
     end    
 endmodule 
