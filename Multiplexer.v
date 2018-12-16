c0af7177f67c83479e53adfb6e5f4448e29f4270
AmirEb <44034763+AmirEb@users.noreply.github.com>
2018-12-16 13:49:09 +0330

    Create Multiplexer.v

---
+++ Multiplexer.v
@@ -0,0 +1,13 @@
+module multiplexer(input1,input2,select,result);
+    input select;
+    input [63:0] input1;
+    input [63:0] input2;
+    output reg [63:0] outcome;
+    always @(input1,input2,select)
+    begin
+    if(select)
+        outcome=input2;
+    else
+        outcome=input1;
+    end    
+endmodule 
