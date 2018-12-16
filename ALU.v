6035ca38425292798d173107269b38908922c12d
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-16 15:44:23 +0330

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,4 +1,4 @@
-module ALU(input1, input2, operation, output );
+module ALU(input1, input2, operation, output1 );
 	input [63:0] num1;
 	input [63:0] num2;
 	input [3:0] operation;
@@ -7,14 +7,14 @@ module ALU(input1, input2, operation, output );
 	always @(num1 , num2 , operation) begin 
 	
 		case(operation)
-			4'b0000:output = input1 & input2;
-			4'b0001:output = input1 | input2;
-			4'b0010:output = input1 + input2;
-			4'b0110:output = input1 - input2;
-			4'b0111:output = input2;
-			4'b1100:output = ~(input1 | input2);
+			4'b0000:output1 = input1 & input2;
+			4'b0001:output1 = input1 | input2;
+			4'b0010:output1 = input1 + input2;
+			4'b0110:output1 = input1 - input2;
+			4'b0111:output1 = input2;
+			4'b1100:output1 = ~(input1 | input2);
 		endcase
    end
-    assign z = ALUout ? 0 : 1;
+    assign z = output1 ? 0 : 1;
 
 endmodule
