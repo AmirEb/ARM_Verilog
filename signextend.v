d6d08acffe55e3a6dd2415f3423dd9af815ebd9d
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-23 16:07:40 +0330

    Update signextend.v

--- signextend.v
+++ signextend.v
@@ -1,8 +1,27 @@
-module signextend(input inp,out);
- 	input [31:0] inp;
-  output [63:0] out;
-  	base on type of instruction
-  assign out = 32(signed(inp));
+module signextend (input_address , output_data);
+  
+        input  [31:0] input_address;
+        output reg [63:0] output_data;
+        
+               always@(input_address)
+begin
+ if (input_address[31:26]==6'b000101)  //BType      
+                     begin 
+                       output_data[25:0] = input_address[25:0];
+                         output_data[63:26] = {38{output_data[25]}};
+			 
+                     end
 
-
+ else if (input_address[31:24] == 8'b10110100) //CBZType
+                     begin 
+                      output_data[18:0] = input_address[23:5];
+                      output_data[63:19] = {65{output_data[18]}};
+                     end 
+          
+               else      //DType
+                     begin  
+                      output_data[8:0] = input_address[20:12];
+                      output_data[63:9] = {55{output_data[8]}};
+                     end
+    end
 endmodule
