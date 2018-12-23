47e328edeae42a04f8c1d45faba38d729f776880
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-23 14:33:07 +0330

    Update Multiplexer.v

--- Multiplexer.v
+++ Multiplexer.v
@@ -1,14 +1,11 @@
 module multiplexer(input1,input2,select,result);
-    parameter delay = 60
+    parameter delay = 60;
     input select;
     input [63:0] input1;
     input [63:0] input2;
     output reg [63:0] outcome;
-    always @(input1,input2,select) without alyas
-    begin
     if(select)
         outcome=input2;
     else
         outcome=input1;
-    end    
 endmodule 
