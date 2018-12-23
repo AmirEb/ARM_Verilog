0ea1101e459e3e4012547081db666f2ac7ab9f00
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-23 14:50:54 +0330

    Update Register.v

--- Register.v
+++ Register.v
@@ -1,14 +1,14 @@
 module Register (data_input,reset,clk)
   parameter delay = 60
 
-  input [n-1 : 0] data_input,
+  input [63 : 0] data_input,
   input reset,
   input clk,
-  output reg [n-1 : 0] data_output
+  output reg [63 : 0] data_output
 
   always @(posedge clk)begin
     if (reset)
-      data_output <= 0 {n{1'b0}};
+      data_output <= 0 {63{1'b0}};
     else
       data_output <= data_input;   
     end    
