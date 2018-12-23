dfb366e03165b1510707a40b992f36b0c3186130
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-23 14:34:14 +0330

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -1,11 +1,10 @@
 module Adder (cin,y,z);
-  parameter n = 64
   parameter delay = 60
 
-  input [n-1:0] z,
-  input [n-1:0] y,
+  input [63:0] z,
+  input [63:0] y,
   input cin,
-  output [n-1:0] x,
+  output [63:0] x,
   output cout;
   
   assign {cout, x} = z + y + cin;
