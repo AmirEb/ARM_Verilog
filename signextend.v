01b0cc2de6401adf647a295740b2e00ce01efd68
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-23 16:23:35 +0330

    Update signextend.v

--- signextend.v
+++ signextend.v
@@ -1,27 +1,15 @@
-module signextend (input_address , output_data);
-  
-        input  [31:0] input_address;
-        output reg [63:0] output_data;
-        
-               always@(input_address)
-begin
- if (input_address[31:26]==6'b000101)  //BType      
-                     begin 
-                       output_data[25:0] = input_address[25:0];
-                         output_data[63:26] = {38{output_data[25]}};
-			 
-                     end
+module SignExtend(instruction, output_data);
 
- else if (input_address[31:24] == 8'b10110100) //CBZType
-                     begin 
-                      output_data[18:0] = input_address[23:5];
-                      output_data[63:19] = {65{output_data[18]}};
-                     end 
-          
-               else      //DType
-                     begin  
-                      output_data[8:0] = input_address[20:12];
-                      output_data[63:9] = {55{output_data[8]}};
-                     end
-    end
+	input [31:0] instruction;
+	output reg [63:0] output_data;
+	wire opcode = instruction[31:30];
+
+	always @ (instruction) begin
+		case(opcode)
+			2'b11: output_data = {{55{instruction[20]}}, instruction[20:12]};
+			2'b10: output_data = {{45{instruction[23]}}, instruction[23:5]};
+			2'b00: output_data = {{38{instruction[25]}}, instruction[25:0]};
+		endcase
+	end
+
 endmodule
