bcc20332cf770db1ebca32ef838297e00206933d
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-30 02:55:07 +0330

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,11 +1,11 @@
-module ALU(input1, input2, operation, output1,z );
-	input [63:0] input1;
-	input [63:0] input2;
+module ALU #(parameter n = 64) (input1, input2, operation, output1,z );
+	input [n-1:0] input1;
+	input [n-1:0] input2;
 	input [3:0] operation;
 	output reg z;
-	output reg [63:0] output;
+	output reg [n-1:0] output1;
 	always @(input1 , input2 , operation) begin 
-	
+	assign z = output_data == 0 ? 1 : 0;
 		case(operation)
 			4'b0000:output1 = input1 & input2;
 			4'b0001:output1 = input1 | input2;
@@ -15,6 +15,5 @@ module ALU(input1, input2, operation, output1,z );
 			4'b1100:output1 = ~(input1 | input2);
 		endcase
    end
-    assign z = output1 ? 0 : 1;
 
 endmodule
