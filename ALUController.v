938835b972195d10d3ccafb070b501b6ef2a49b7
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-30 02:37:17 +0330

    Update ALUController.v

--- ALUController.v
+++ ALUController.v
@@ -1,19 +1,34 @@
-module ALUController(ALUOp, Opcode, code);
-	
-	input [1:0] ALUOp;
-	input [10:0] OpCode;
-	output reg [3:0] code;
-	always @(ALUOp, OpCode) begin
-		 case(ALUOp) 
-		     2'b00: code = 4'b0010;
-		     2'b01: code = 4'b0111;
-		     2'b10:
-			casex(OpCode)
-				11'b11xxxxxxxxx: code = 4'b0110;
-				11'b101xxxxxxxx: code = 4'b0001;
-				11'b10001010xxx: code = 4'b0000;
-				11'b10001011xxx: code = 4'b0010;
-			endcase
-		  endcase
-	end
-endmodule 
+
+module ALUControl (ALUOp0, ALUOp1, instruction_part, operation_code);
+
+    input ALUOp0, ALUOp1;
+    input [10:0] instruction_part;
+    output reg [3:0] operation_code;
+
+    wire [1:0] ALUOp = {ALUOp1, ALUOp0};
+
+    always @ (ALUOp0, ALUOp1, instruction_part) begin
+
+        casex(ALUOp)
+        
+            2'b00: operation_code = 4'b0010;
+            2'bx1: operation_code = 4'b0111;
+            2'b1x: begin
+
+                    11'b10001011000: operation_code = 4'b0010;
+                    11'b11001011000: operation_code = 4'b0110;
+                    11'b10001010000: operation_code = 4'b0000;
+                    11'b10101010000: operation_code = 4'b0001;
+                    default: operation_code = 4'bz;
+                
+                endcase
+            end
+            default: operation_code = 4'bz;
+
+        endcase
+
+    end
+
+endmodule
+
+
