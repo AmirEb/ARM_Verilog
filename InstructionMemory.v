28ac4d44c6ec718c5adeef50513cf3028cd897cc
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2018-12-30 02:47:05 +0330

    Update InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -6,6 +6,28 @@ module InstructionMemory( input_address , output_data);
 	output [31:0] output_data;
 
 	reg [7:0] memory [0:255];
+	
+	
+		initial begin
+		memory[0] <= 8'hE5;
+		memory[1] <= 8'h03;
+		memory[2] <= 8'h1F;
+		memory[3] <= 8'h8B;
+		
+		memory[4] <= 8'hA4;
+		memory[5] <= 8'h00;
+		memory[6] <= 8'h40;
+		memory[7] <= 8'hF8;
+		
+		memory[8] <= 8'h86;
+		memory[9] <= 8'h00;
+		memory[10] <= 8'h04;
+		memory[11] <= 8'h8B;
+		
+		memory[12] <= 8'hA6;
+		memory[13] <= 8'h10;
+		memory[14] <= 8'h00;
+		memory[15] <= 8'hF8;
 
 	assign output_data[7 : 0] = memory[input_address];
 	assign output_data[15 : 8] = memory[input_address + 1];
